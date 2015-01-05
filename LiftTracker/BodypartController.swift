d1934782672bad5c8a80f0b80c928703845edc52
Tommy Fannon <tfannon@gmail.com>
2015-01-05 06:09:22 -0500

    lifttracker

--- LiftTracker/BodypartController.swift
+++ LiftTracker/BodypartController.swift
@@ -13,6 +13,20 @@ class BodypartController: UICollectionViewController {
     let bodyparts = ["Chest", "Shoulders", "Arms", "Back", "Legs", "Other"]
     let cellIdentifier = "BodypartCell"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var imageView = UIImageView(frame:self.view.bounds)
+        imageView.contentMode = UIViewContentMode.ScaleToFill
+        imageView.image = UIImage(named: "dumbells.jpg")!
+        self.view.insertSubview(imageView, atIndex: 0)
+        
+        let addButton = UIButton(frame: CGRectMake(0, UIScreen.mainScreen().bounds.size.height - 88, UIScreen.mainScreen().bounds.size.width, 44))
+        addButton.setTitle("+", forState: .Normal)
+        addButton.backgroundColor = UIColor(red: 0.5, green: 0.9, blue: 0.5, alpha: 1.0)
+        addButton.addTarget(self, action: "addNewItem", forControlEvents: .TouchUpInside)
+        self.view.addSubview(addButton)
+    }
+    
     
      // MARK: UICollectionViewDataSource
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
