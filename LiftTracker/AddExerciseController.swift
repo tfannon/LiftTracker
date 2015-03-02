8b2c3a1ffb96663c959140557748c12747b37e07
Tommy Fannon <tfannon@gmail.com>
2015-03-02 13:15:30 -0500

    added the addExercise controller

---
+++ LiftTracker/AddExerciseController.swift
@@ -0,0 +1,12 @@
+//
+//  AddExerciseController.swift
+//  LiftTracker
+//
+//  Created by Tommy Fannon on 3/2/15.
+//  Copyright (c) 2015 Crazy8Dev. All rights reserved.
+//
+import UIKit
+
+class AddExerciseController: UITableViewController {
+
+}
