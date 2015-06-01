a3324e1cd1d105168e4a73ac027a67b24b20bbb8
tfannon <tfannon@gmail.com>
2015-05-31 23:56:01 -0400

    realm + json

--- LiftTracker/RealmModel.swift
+++ LiftTracker/RealmModel.swift
@@ -7,12 +7,27 @@
 //
 
 import RealmSwift
+import Foundation
 
-class RBodypart : Object {
+public class RBodypart : Object {
     dynamic var name = ""
+    dynamic var displayOrder = 0
+    dynamic var isSystem = true
     let exercises = List<RExercise>()
 }
 
 public class RExercise : Object {
     dynamic var name = ""
+    dynamic var isSystem = true
+    let prs = List<PR>()
+    
+    override public static func primaryKey() -> String? {
+        return "name"
+    }
+}
+
+public class PR : Object {
+    dynamic var rep : Int = 0
+    dynamic var weight : Int = 0
+    dynamic var date = NSDate()
 }
--- LiftTrackerTests/RealmTests.swift
+++ LiftTrackerTests/RealmTests.swift
@@ -51,9 +51,42 @@ class RealmTests: XCTestCase {
         }
     }
     
+    func testPRs() {
+        let exercise = RExercise()
+        exercise.name = "Squat"
+        let pr1 = PR()
+        pr1.rep = 6
+        pr1.weight = 125
+        exercise.prs.append(pr1)
+        
+        let pr2 = PR()
+        pr2.rep = 5
+        pr2.weight = 125
+        exercise.prs.append(pr2)
+        
+        let exercise2 = RExercise()
+        exercise2.name = "Bench Press"
+        let pr3 = PR()
+        pr3.rep = 8
+        pr3.weight = 95
+        pr3.date = NSDate().dateBySubtractingDays(4)
+        exercise2.prs.append(pr3)
+        
+        
+        let realm = Realm()
+        realm.write {
+            realm.add(exercise)
+            realm.add(exercise2)
+        }
+    }
+    
+    
     func testBodypartExercises() {
         let bodypart = RBodypart()
         bodypart.name = "Chest"
+        
+        let bodypart2 = RBodypart()
+        bodypart2.name = "Triceps"
         
         let exercise = RExercise()
         exercise.name = "Dumbell presses"
@@ -62,20 +95,39 @@ class RealmTests: XCTestCase {
         exercise2.name = "Bench Press"
         
         bodypart.exercises.extend([exercise, exercise2])
+        bodypart2.exercises.append(exercise2)
         
         let realm = Realm()
         realm.write {
             realm.add(bodypart)
+            realm.add(bodypart2)
             realm.add(exercise)
             realm.add(exercise2)
         }
         
         let exercises = Realm().objects(RExercise)
         let bodyparts = Realm().objects(RBodypart)
-        XCTAssertEqual(1, bodyparts.count)
+        XCTAssertEqual(2, bodyparts.count)
         XCTAssertEqual(2, exercises.count)
         XCTAssertEqual(2, bodypart.exercises.count)
+        XCTAssertEqual(1, bodypart2.exercises.count)
 
     }
+    
+    
+    func testJson() {
+        let realm = Realm()
+        var error: NSError? = nil
+        let jsonURL = NSBundle.mainBundle().URLForResource("Exercise", withExtension: "json")
+        let jsonData = NSData(contentsOfURL: jsonURL!)
+        let jsonArray = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: &error) as! NSArray
+        realm.beginWrite()
+        for x in jsonArray {
+            realm.create(RExercise.self, value: x, update: true)
+        }
+        realm.commitWrite()
+    }
+    
+    
 }
         
